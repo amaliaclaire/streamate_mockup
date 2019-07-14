36fa8853b886a0e9f71381bf73d38d2ff5209818
Amalia <amalia.fergin@gmail.com>
2019-07-13 21:35:48 -0700

    semantic ui react

---
+++ src/components/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+class LandingPage extends React.Component {
+  render () {
+    return (
+      <div>Landing Page is here</div>
+    )
+  }
+};
+
+export default LandingPage;
